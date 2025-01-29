97b170346167870a0bcf1e6de62098550bd50af8
rbmm <_dm_sh_@tutanota.com>
2025-01-29 21:21:01 +0200

    Update pch.h

--- pch.h
+++ pch.h
@@ -1,3 +1,50 @@
+#pragma once
+
+#define DECLSPEC_DEPRECATED_DDK
+
+#define _CRT_SECURE_NO_DEPRECATE
+#define _CRT_NON_CONFORMING_SWPRINTFS
+#define _NO_CRT_STDIO_INLINE
+#define _CRT_SECURE_CPP_OVERLOAD_SECURE_NAMES 0
+#define _ALLOW_COMPILER_AND_STL_VERSION_MISMATCH
+#define __EDG__
+#define USE_ATL_THUNK2
+
+#define CMSG_SIGNED_ENCODE_INFO_HAS_CMS_FIELDS
+#define CMSG_SIGNER_ENCODE_INFO_HAS_CMS_FIELDS
+
+#ifndef DECLSPEC_IMPORT
+#define DECLSPEC_IMPORT __declspec(dllimport)
+#endif
+
+#define DPAPI_IMP DECLSPEC_IMPORT
+#define _CRTIMP DECLSPEC_IMPORT
+#define _CRTIMP_ALT DECLSPEC_IMPORT
+
+#define _NT_BEGIN namespace NT {
+#define _NT_END }
+
+#pragma warning(disable : 4073 4074 4075 4097 4514 4005 4200 4201 4238 4307 4324 4392 4471 4480 4530 4706 5040)
+
+#include <stdlib.h>
+// #include <wchar.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <WinSock2.h>
+#include <intrin.h>
+#include <windowsx.h>
+#undef WIN32_NO_STATUS
+#include <ntstatus.h>
+#include <winioctl.h>
+
+//#include <atlbase.h>
+//#include <atlwin.h>
+
+typedef GUID *PGUID;
+
+EXTERN_C IMAGE_DOS_HEADER __ImageBase;
+
 #ifndef PHNT_MODE
 #define PHNT_MODE PHNT_MODE_USER
 #endif
@@ -10,31 +57,13 @@
 #define SECURITY_WIN32
 #endif
 
-#pragma warning(disable : 4073 4074 4075 4097 4514 4005 4200 4201 4238 4307 4324 4471 4480 4530 4706 5040)
+//#define _NTLSA_
 
-#define DECLSPEC_DEPRECATED_DDK
+#define PHNT_NO_INLINE_INIT_STRING
 
-#define _CRT_SECURE_NO_DEPRECATE
-#define _CRT_NON_CONFORMING_SWPRINTFS
-#define _NO_CRT_STDIO_INLINE
-#define _CRT_SECURE_CPP_OVERLOAD_SECURE_NAMES 0
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-
-#include <WinSock2.h>
-#include <intrin.h>
-#include <windowsx.h>
-#undef WIN32_NO_STATUS
-#include <ntstatus.h>
-#include <winioctl.h>
-
-typedef GUID *PGUID;
-
-EXTERN_C IMAGE_DOS_HEADER __ImageBase;
-
+#pragma warning(disable : 4073 4074 4075 4097 4514 4005 4200 4201 4238 4307 4324 4392 4471 4480 4530 4706 5040)
 #include "phnt.h"
+#pragma warning(default : 4392)
 
 #define MINCHAR     0x80        // winnt
 #define MAXCHAR     0x7f        // winnt
@@ -45,4 +74,3 @@ EXTERN_C IMAGE_DOS_HEADER __ImageBase;
 #define MAXUCHAR    0xff        // winnt
 #define MAXUSHORT   0xffff      // winnt
 #define MAXULONG    0xffffffff  // winnt
-
